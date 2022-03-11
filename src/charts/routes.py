367128a8f8ee3ce21db9660f6f452568ec96d842
Josh Abraham <josh.m.n.abraham@gmail.com>
2022-03-10 22:18:02 -0500

    Revert "Revert "remove unused A1""
    This reverts commit 8e6f077682b0f588a2047798ed87f04e0192f1b0.

--- src/charts/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template
-from charts import webapp
-
-@webapp.route('/')
-@webapp.route('/home')
-def home():
-    """ Main route, as well as default location for 404s
-    """
-    return render_template("index.html")
--- src/memcache_app/config.py
+++
@@ -1,4 +0,0 @@
-from cachetools import LRUCache, RRCache
-global memcache_obj
-''' This will act as a global variable between main thread and background job's thread
-'''
