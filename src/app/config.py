3a95c89f1f678a46abbc3b9b0f5f7c181a254b69
vishal kanna <vishalkanna93@gmail.com>
2022-03-11 19:15:17 -0500

    s3 dev

--- src/app/config.py
+++ src/app/config.py
@@ -1,7 +1,7 @@
 db_config = {'user': 'root',
-             'password': 'Password123#',
+             'password': 'ece1779pass',
              'host': '127.0.0.1',
              'database': 'ImageStore'}
-#ece1779pass
+#
 max_capacity = 2
 replacement_policy = 'Least Recently Used'
