cafde857f43efcb8f1d0d6143d8fa2748df8feea
Josh Abraham <josh.m.n.abraham@gmail.com>
2022-03-18 01:11:55 -0400

    Update ImageStore.sql

--- manager_server/database/ImageStore.sql
+++ manager_server/database/ImageStore.sql
@@ -20,3 +20,11 @@ CREATE TABLE cache_stats(
       miss_count INT NOT NULL,
       PRIMARY KEY (created_at)
       );
+CREATE TABLE cache_policy (
+    param_key INT NOT NULL AUTO_INCREMENT,
+    max_miss_rate INT NOT NULL,
+    min_miss_rate INT NOT NULL,
+    exp_ratio INT NOT NULL,
+    shrink_ratio INT NOT NULL,
+    PRIMARY KEY (param_key)
+);
